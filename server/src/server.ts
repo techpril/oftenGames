3b6444e624bba70a18fc4c968defa035b1cc489e
techpril <priscila.teodorio2015@gmail.com>
2023-01-02 15:58:50 -0300

    add typescript e home

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+
+import express from 'express'
+
+const app = express()
+
+app.get('/home', (request, response) => {
+    return response.json([
+        {id: 1, name:'Teste 1'},
+        {id: 2, name:'Teste 2'},
+        {id: 3, name:'Teste 3'},
+
+    ])
+})
+
+app.listen(3333)
